c8ce709b3ff26946fd9e5fcbe50376934bea3946
jbandura <jacek.bandura@netguru.pl>
2016-06-28 09:33:56 +0200

    fix small issues

--- app/js/components/PanelSwitcher.jsx
+++ app/js/components/PanelSwitcher.jsx
@@ -6,7 +6,7 @@ export default (props) => {
   const inactiveClass = `${cssNamespace}__item ${cssNamespace}__item--last`;
   if(props.activePane === 'talent') {
     return (
-      <div className="clearfix">
+      <div className="c-panel-switcher clearfix">
         <div className={activeClass}>I'm a Talent</div>
         <a href="/invite_requests/new" className={inactiveClass}>I'm an Employer</a>
       </div>
@@ -14,7 +14,7 @@ export default (props) => {
   }
 
   return (
-    <div className="clearfix">
+    <div className="c-panel-switcher clearfix">
       <a href="/users/sign_up" className={inactiveClass}>I'm a Talent</a>
       <div className={activeClass}>I'm an Employer</div>
     </div>
