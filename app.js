be87ae1a52db78920bfd969cdd1a8125d9091467
jbandura <jacek.bandura@netguru.pl>
2016-06-30 18:15:25 +0200

    apply fixes

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ var yaml    = require('js-yaml');
 var fs      = require('fs');
 var app     = express();
 
-app.use(express.static('public', {'setHeaders': setHeaders}));
-
-function setHeaders(res, path) {
-  var regexp = /\.(.+)$/;
-  if(!regexp.test(path)) {
-    res.setHeader('Content-type', 'text/html');
+app.use(express.static('public', {
+  setHeaders: function(res, path) {
+    var regexp = /\.(.+)$/;
+    if(!regexp.test(path)) {
+      res.setHeader('Content-type', 'text/html');
+    }
   }
-}
+}));
 
 app.listen(4000, function () {
   console.log('Running app on port 4000!');
