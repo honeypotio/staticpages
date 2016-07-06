f86e24fa803622555e44d3d2bf7b73094b5f9602
jbandura <jacek.bandura@netguru.pl>
2016-07-06 16:49:14 +0200

    implement responsive images

--- app/initialize.jsx
+++ app/initialize.jsx
@@ -29,9 +29,14 @@ const showNavigationLink = (page) => {
   }
 };
 
+const initResponsiveImages = () => {
+  $('img.hisrc').hisrc();
+};
+
 $(document).ready(() => {
   showNavigationLink(window.location.pathname);
   const flashMessagesService = new FlashMessages($('#flash-messages'));
   flashMessagesService.showCurrent();
   load();
+  initResponsiveImages();
 });
