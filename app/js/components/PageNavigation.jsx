d410426ccc0a85fcc3f455301077fe6d6a30b376
Kado <kadodamball@hotmail.com>
2017-07-17 15:09:40 +0200

    Readd blog link as a main link

--- app/js/components/PageNavigation.jsx
+++ app/js/components/PageNavigation.jsx
@@ -24,14 +24,13 @@ const showLinks = () => {
   if(!userSession.isLoggedIn()) {
     links = [
       ...showDeveloperEmployersLink(),
+      <li key="blog"><a href="http://blog.honeypot.io/">Blog</a></li>,
       <li key="about" className="c-navigation__mobile"><a href={buildUrl('/pages/about')}>About</a></li>,
-      <li key="blog" className="c-navigation__mobile"><a href="http://blog.honeypot.io/">Blog</a></li>,
       <li key="autotech" className="c-navigation__mobile"><a href="https://honeypotio.github.io/research/">Research</a></li>,
       <li key="dropdown" className="c-navigation__company dropdown">
         <a href="#" className="dropdown-toggle text-primary" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Company <span className="caret"></span> </a>
         <ul className="c-navigation__dropdown  dropdown-menu">
           <li><a href={buildUrl('/pages/about')}>About</a></li>
-          <li><a href="http://blog.honeypot.io/">Blog</a></li>
           <li><a href="https://honeypotio.github.io/research/">Research</a></li>
         </ul>
       </li>,
