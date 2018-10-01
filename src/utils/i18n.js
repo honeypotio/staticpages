76839c70154ca43716bd9c1df5c43ad3f8c1f9d6
Kado <kadodamball@hotmail.com>
2018-10-01 15:33:05 +0200

    Log current language

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -9,6 +9,7 @@ import faq from '../locales/faq.json';
 
 export const languages = ['en', 'de', 'nl'];
 const path = (typeof window !== 'undefined' ? window.location.pathname.replace(process.env.URL_BASE, '') : '');
+console.log(path, path.split('/')[1])
 const defaultLang = 'en';
 
 i18n.init({
